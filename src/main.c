cbebeeda3b905a0023138d54b8f252058f0a3d37
lucas <2238019055@qq.com>
2026-03-15 16:53:43 +0800

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -6,7 +6,6 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
 
   char command[256];
@@ -14,8 +13,6 @@ int main(int argc, char *argv[]) {
 
   // 去除多余的换行符
   command[strcspn(command, "\r\n")] = 0;
-
-  // 简单的命令处理逻辑
   printf("%s: command not found\r\n", command);
   return 0;
 }
