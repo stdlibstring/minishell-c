35df5b72517abfbd1a9d00861d0b53e81dd6393a
lucas <2238019055@qq.com>
2026-03-15 16:51:46 +0800

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -8,5 +8,13 @@ int main(int argc, char *argv[]) {
   // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
 
+  char command[256];
+  fgets(command, sizeof(command), stdin);
+
+  // 去除多余的换行符
+  command[strcspn(command, "\r\n")] = 0;
+
+  // 简单的命令处理逻辑
+  printf("%s: command not found\r\n", command);
   return 0;
 }
